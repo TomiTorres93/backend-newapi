98a3f0e84cdba9fc45defa191dd249164c41045c
tomi torres <tomitorres.93@gmail.com>
2023-12-29 13:02:49 -0300

    mongo

--- src/config/mongo.config.mjs
+++ src/config/mongo.config.mjs
@@ -3,10 +3,10 @@ import mongoose from 'mongoose';
 
 export default  async function  connectMongoDB  () {
   try {
-      await mongoose.connect('mongodb+srv://tomitorres93:<rodolfowalsh93>@cluster0.xhrxoec.mongodb.net/?retryWrites=true&w=majority');
+      await mongoose.connect('mongodb+srv://tomitorres93:rodolfowalsh93@cluster0.xhrxoec.mongodb.net/?retryWrites=true&w=majority');
       console.log("Conectado con éxito a MongoDB usando Mongoose.");
   } catch (error) {
-      
+      console.log("No te pudiste conectar usando Mongoose")
  
   }
 };
