015b157df2534f1488b9f4656b7ae8e147fc2833
tomi torres <tomitorres.93@gmail.com>
2023-12-29 00:19:17 -0300

    cors

--- src/server.mjs
+++ src/server.mjs
@@ -47,7 +47,9 @@ const corsOptions = {
 
 
 app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Allow-Credentials', true);
   next();
 });
 
